6e4c17842ef46056d05b39dadde556f74cedffa9
Shekhar Kushwaha <151432774+flyhighr@users.noreply.github.com>
2025-02-20 13:55:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -603,7 +603,7 @@ async def resend_verification(
         
         if user.get("is_verified", False):
             raise HTTPException(
-                status_code=status.HTTP_400_BAD_REQUEST,
+                status_code=status.HTTP_400_BROKEN,
                 detail="Email already verified"
             )
         
