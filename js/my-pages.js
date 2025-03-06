991f24a78ba1e66c9fe296862ac1cdd2446f3a1d
Shekhar Kushwaha <151432774+flyhighr@users.noreply.github.com>
2025-03-06 09:22:14 +0530

    Update my-pages.js

--- js/my-pages.js
+++ js/my-pages.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const API_URL = 'https://versz-vs1b.onrender.com';
+    const API_URL = 'https://api.versz.fun';
     let allPages = [];
     let cachedViews = {};
     
